dc43e184fa21961f98ce6111d135a45bc8f0d996
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-03-13 22:06:25 -0300

    versão 2.0 - verificação de cpf, correção no while referente a idade

--- cadastro_banco.py
+++ cadastro_banco.py
@@ -19,7 +19,7 @@ while input_criar_conta_valido == False:
         print('Vamos precisar de algumas informações suas')
 
         nome_valido = False
-        while nome_valido == False:
+        while nome_valido == False:  # pedindo e verificando o nome do usuário
             nome = input('Digite o seu nome: ')
 
             print('')
@@ -36,19 +36,17 @@ while input_criar_conta_valido == False:
         print('Lembre-se que é preciso ser maior de idade para abrir conta :)')
 
         idade_valida = False
-        while idade_valida == False:
+        while idade_valida == False:    # pedindo a idade e verificando se é maior de idade
             
             idade = input('Digite a sua idade: ')
             if idade.isnumeric():
                 idade_int = int(idade)
-                
-                if type(idade_int) == int:
-                    print('ok')
-                
+
                 if idade_int >= 18:
                     print('Você é maior de idade e pode se cadastrar! ')
-                    print('Agora só falta um passo! ')
-                    ano_nascimento_valida = True
+                    print('Estamos quase lá!')
+                    idade_valida = True
+
                 else:
                     print('Não é possível se cadastrar sendo menor de idade :(')
                     break
@@ -56,9 +54,21 @@ while input_criar_conta_valido == False:
                 print('Digite apenas números')    
                 continue
             
+        print('')
 
-           
+        print('-'* 50)
 
+        cpf_valido = False
+        while cpf_valido == False:  # cpf do usuario e verificação de indices
+
+            cpf = input('Digite o seu CPF: ')
+            if cpf.isnumeric():
+                cpf_int = int(cpf)
+
+            if len(cpf) == 11:
+                print(cpf[0:3], cpf[3:6], cpf[6:9],cpf[9:], sep='.', end='-')
+                print('CPF válido para cadastro.')
+                cpf_valido = True
 
     elif input_criar_conta == 'não':
         print('Que pena :( ... estaremos aqui caso mude de ideia')
@@ -69,3 +79,13 @@ while input_criar_conta_valido == False:
     else:
         input_criar_conta_valido = False
         print('Digite apenas de acordo com as opções acima')
+
+'''
+
+
+print(999, 888, 777, sep='.', end='-')
+print(66)
+
+# 012345678910  
+# 12432777930
+'''
